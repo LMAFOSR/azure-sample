79052faa3e3a6c94f0843f0b35741a32c26cdfe0
Josh Stearns <sjstearns@gmail.com>
2021-01-09 00:09:46 -0500

    Initialize repository

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express()
+const port = process.env.PORT || 8081;
+
+app.get('/', (req, res) => {
+  res.send('I am listenning')
+})
+
+app.listen(port, () => {
+  console.log(`I am running on ${port}`)
+})
