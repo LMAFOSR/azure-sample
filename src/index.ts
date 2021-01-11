310cffa98626213843002d68442e3d03e5ebcf03
Josh Stearns <sjstearns@gmail.com>
2021-01-10 19:05:00 -0500

    Update

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 8081;
 
 app.get('/', (req, res) => {
-  res.send(`I am listenning ${process.env.TEST} !!! ${process.env.ACCOUNTNUMBER} @@@`)
+  res.send(`I am listenning ${process.env.TEST} !!! ${process.env.ACCOUNTNUMBER} @@@ HHHH`)
 });
 
 app.listen(port, () => {
